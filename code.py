d4e7c820eedcca3f53c1a5d1eb10e091e55ba01a
AdithyaBA21710 <adithya.ba21710@gmail.com>
2025-11-12 22:38:40 +0530

    Update code.py

--- code.py
+++ code.py
@@ -5,8 +5,8 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
 iris = load_iris()
-X = iris.data        # features (like petal length, width, etc.)
-y = iris.target      # labels (flower types)
+X = iris.data        
+y = iris.target      
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
@@ -14,7 +14,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
 
-predictions = model.predict(X_test)
 
-accuracy = accuracy_score(y_test, predictions)
-print (accuracy )
+new_flower = [[5.1, 3.5, 1.4, 0.2]]
+prediction = model.predict(new_flower)
+print("Predicted species:", iris.target_names[prediction][0])
