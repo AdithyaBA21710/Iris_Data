8d7d9f16789da95508aacd65e41800119edb0782
AdithyaBA21710 <adithya.ba21710@gmail.com>
2025-11-12 21:41:57 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+iris = load_iris()
+X = iris.data        # features (like petal length, width, etc.)
+y = iris.target      # labels (flower types)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, predictions)
+print (accuracy )
